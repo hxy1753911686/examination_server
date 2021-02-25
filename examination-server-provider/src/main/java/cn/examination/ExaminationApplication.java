030b569c4ce8044f043d92b7f674c7e4282f6f19
hxy1753911686 <1753911686@qq.com>
2021-02-26 00:14:08 +0800

    解决打包问题

--- examination-server-provider/src/main/java/cn/examination/ExaminationApplication.java
+++ examination-server-provider/src/main/java/cn/examination/ExaminationApplication.java
@@ -4,12 +4,21 @@ import cn.examination.config.configuration.AuthRsaKeyProperties;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("cn.examination.config.mapper")
 @EnableConfigurationProperties(AuthRsaKeyProperties.class)
-public class ExaminationApplication {
+public class ExaminationApplication{
+//public class ExaminationApplication extends SpringBootServletInitializer {
+
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(ExaminationApplication.class);
+//    }
+
     public static void main(String[] args) {
         SpringApplication.run(ExaminationApplication.class,args);
     }
